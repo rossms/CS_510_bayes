3093be19d01ecb0bb323f80cc1d6ab5df0af94a8
Ross <silberquitr@gmail.com>
2017-10-21 13:13:48 -0400

    file deleter

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ Bayes = Bayes_Classifier()
 testFile = Bayes.loadFile('./test.txt')
 #print(testFile)
 #print(Bayes.countTokens(Bayes.tokenize(testFile)))
+try:
+    os.remove('./negDictionary.txt')
+except OSError:
+    pass
+try:
+    os.remove('./posDictionary.txt')
+except OSError:
+    pass
 #Bayes.train()
-negDictionary = Bayes.load('./negDictionary.txt')
-posDictionary = Bayes.load('./posDictionary.txt')
+#negDictionary = Bayes.load('./negDictionary.txt')
+#posDictionary = Bayes.load('./posDictionary.txt')
 
 #print(posDictionary)
